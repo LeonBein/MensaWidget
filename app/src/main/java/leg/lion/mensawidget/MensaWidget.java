4e106df5fb165de5e280ad6e24397966a45c9563
Leon Bein <leon.bein@student.hpi.de>
2018-10-28 18:02:24 +0100

    Widget does not crash anymore when no internet connection

--- app/src/main/java/leg/lion/mensawidget/MensaWidget.java
+++ app/src/main/java/leg/lion/mensawidget/MensaWidget.java
@@ -6,6 +6,8 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.TextView;
@@ -28,7 +30,7 @@ public class MensaWidget extends AppWidgetProvider {
      * Updatefrequenz anpassen
      */
 
-
+    private static String lastUpdate = "never";
 
     public static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
 
@@ -38,7 +40,8 @@ public class MensaWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.mensa_widget);
         views.setTextViewText(R.id.header, headerText);
         views.setTextViewText(R.id.appwidget_text, widgetText);
-        views.setTextViewText(R.id.footer, "Last updated at "+timeFormat.format(new Date()));
+        lastUpdate = timeFormat.format(new Date());
+        views.setTextViewText(R.id.footer, "Last updated at "+lastUpdate);
         Random rnd = new Random();
         int color = Color.argb(255, rnd.nextInt(100), rnd.nextInt(100), rnd.nextInt(100));
         //views.setInt(R.id.appwidget_text, "setBackgroundColor", color);
@@ -57,14 +60,47 @@ public class MensaWidget extends AppWidgetProvider {
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    static void errorAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId, String errorMessage) {
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.mensa_widget);
+        views.setTextViewText(R.id.footer, errorMessage+" \tLast updated at "+lastUpdate);
+
+        Intent intentUpdate = new Intent(context, MensaWidget.class);
+        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] idArray = new int[]{appWidgetId};
+        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+        PendingIntent pendingUpdate = PendingIntent.getBroadcast(
+                context, appWidgetId, intentUpdate,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        //Log.d("==============Received:","Update======================================");
-        String headerText = Requester.header();
-        String widgetText = Requester.fullString();
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId, headerText, widgetText);
+
+        //Log.d("==============Received:", "Update======================================");
+        try {
+            String headerText = Requester.header();
+            String widgetText = Requester.fullString();
+            // There may be multiple widgets active, so update all of them
+            for (int appWidgetId : appWidgetIds) {
+                updateAppWidget(context, appWidgetManager, appWidgetId, headerText, widgetText);
+            }
+        } catch (Requester.RequesterException e) {
+            e.printStackTrace();
+            //Log.e("Erri", e.getMessage(), e);
+            for (int appWidgetId : appWidgetIds) {
+                errorAppWidget(context, appWidgetManager, appWidgetId, e.getMessage());
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+            //Log.e("Erri2", e.getMessage(), e);
+            for (int appWidgetId : appWidgetIds) {
+                errorAppWidget(context, appWidgetManager, appWidgetId, "Unidentified error");
+            }
         }
     }
 
--- app/src/main/java/leg/lion/mensawidget/Requester.java
+++ app/src/main/java/leg/lion/mensawidget/Requester.java
@@ -13,6 +13,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.UnknownHostException;
@@ -27,18 +28,25 @@ import java.util.stream.Collectors;
 
 public class Requester {
 
+    public static class RequesterException extends Exception {
+        public RequesterException(String message, Throwable cause){
+            super(message, cause);
+        }
+    }
+
 
 	public static final SimpleDateFormat parseFormat = new SimpleDateFormat("yyyy-MM-dd");
 	public static final SimpleDateFormat printFormat = new SimpleDateFormat("EEEE, d. MMMM", Locale.GERMAN);
 
 
 
-	public static StringBuilder requestWebsite(String url) throws IOException {
+	public static StringBuilder requestWebsite(String url) throws IOException, RequesterException {
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 		StrictMode.setThreadPolicy(policy);
         URL website = new URL(url);
         URLConnection yc = website.openConnection();
-		yc.setConnectTimeout(5000);
+		yc.setConnectTimeout(2000);
+		yc.setReadTimeout(2000);
         StringBuilder resultSb = new StringBuilder();
         try(BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()))){
             String inputLine;
@@ -47,50 +55,46 @@ public class Requester {
             }
         } catch(IOException io) {
 			io.printStackTrace();
-			if(io instanceof UnknownHostException)throw io;
+			if(io instanceof SocketTimeoutException || io instanceof UnknownHostException)throw new RequesterException("Could not connect to website",io);
         	return null;
         }
         return resultSb;
 	}
 
-	public static String header(){
+	public static String header() throws RequesterException {
 		String result = "";
 		try {
 			String dateString = nextDate();
 			Date date = parseFormat.parse(dateString);
 			result = printFormat.format(date);
 		} catch (IOException e) {
-			e.printStackTrace();
-			result = "Error while connecting to website";
+            throw new RequesterException("Error while connecting to website", e);
 		} catch (ParseException | JSONException e) {
-			e.printStackTrace();
-			result = "Error while parsing";
+            throw new RequesterException("Error while parsing", e);
 		}
 		return result;
 	}
 
-	public static String fullString(){
+	public static String fullString() throws RequesterException {
 		String result = "";
 		try {
 
 			for(String meal : meals(nextDate()))result += meal+"\n\n";
 			result = result.substring(0,result.length()-1);
 		} catch (IOException e) {
-			e.printStackTrace();
-			result = "Error while connecting to website";
+            throw new RequesterException("Error while connecting to website", e);
 		} catch (JSONException e) {
-			e.printStackTrace();
-			result = "Error while parsing";
+            throw new RequesterException("Error while parsing", e);
 		}
 		return result;
 	}
 
-	public static String nextDate() throws IOException, JSONException {
+	public static String nextDate() throws IOException, JSONException, RequesterException {
 		String resp = requestWebsite("https://openmensa.org/api/v2/canteens/62/days").toString();
 		return ((JSONObject)new JSONArray(resp).get(0)).get("date").toString();
 	}
 
-	public static List<String> meals(String date) throws IOException, JSONException {
+	public static List<String> meals(String date) throws IOException, JSONException, RequesterException {
 		String resp = requestWebsite("https://openmensa.org/api/v2/canteens/62/days/"+date+"/meals").toString();///2018-08-06/meals");
 		JSONArray meals = new JSONArray(resp);
 		List<String> list = new ArrayList<String>();
